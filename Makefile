ee6b96d0e502a4337e1d8277dd67b8d4f3bdfbde
Hsiaoming Yang <me@lepture.com>
2023-02-28 17:59:22 +0900

    chore: add script to bump version

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 build-docs:
 	sphinx-build docs build/_html -a -b dirhtml
+
+build-icons:
+	node scripts/genicons.js
---
+++ scripts/versionbump.py
@@ -0,0 +1,27 @@
+import re
+import datetime
+
+
+def create_version():
+    today = datetime.date.today()
+    numbers = [today.year, today.month, today.day]
+    return '.'.join(str(i) for i in numbers)
+
+
+def replace_version(version):
+    filepath = 'src/shibuya/__init__.py'
+
+    with open(filepath, 'r') as f:
+        content = f.read()
+        code = re.sub(r'__version__ = "[^"]+"', f'__version__ = "{version}"', content)
+
+    with open(filepath, 'w') as f:
+        f.write(code)
+
+
+def bump():
+    version = create_version()
+    replace_version(version)
+
+
+bump()
