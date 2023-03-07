874063df451dc03a33c9254e0d87197c33a3ddd5
Hsiaoming Yang <me@lepture.com>
2023-03-07 20:39:27 +0900

    fix: improve style for repo stats

--- static/js/repo-stats.js
+++ static/js/repo-stats.js
@@ -1,39 +1,52 @@
-async function fetchGitHubStats (repo) {
-  const url = `https://api.github.com/repos/${repo}`
-  const resp = await fetch(url)
-  const data = await resp.json()
-  renderStats(data.watchers, data.forks)
-}
-
-
-function renderStats (stars, forks) {
-  localStorage.setItem('_stars', stars)
-  localStorage.setItem('_forks', forks)
-  localStorage.setItem('_time', Date.now())
-  showStats(stars, forks)
-}
-
-
-function showStats (stars, forks) {
-  document.querySelector('.js-repo-stars').textContent = stars
-  document.querySelector('.js-repo-forks').textContent = forks
-}
-
 const element = document.querySelector('.js-repo-stats')
 
-if (element) {
-  const stars = localStorage.getItem('_stars')
-  const forks = localStorage.getItem('_forks')
-  if (stars || forks) {
-    showStats(stars, forks)
+
+async function fetchGitHubStats (user, repo) {
+  const url = `https://api.github.com/repos/${user}/${repo}`
+  const resp = await fetch(url)
+  const data = await resp.json()
+  const stats = {stars: data.watchers, forks: data.forks}
+  showStats(stats)
+  sessionStorage.setItem('_sy/repo/stats', JSON.stringify(stats))
+}
+
+
+async function fetchGitlabStats (user, repo) {
+  const url = 'https://gitlab.com/api/v4/projects/' + encodeURIComponent(user + '/' + repo)
+  const resp = await fetch(url)
+  const data = await resp.json()
+  const stats = {stars: data.star_count, forks: data.forks_count}
+  showStats(stats)
+  sessionStorage.setItem('_sy/repo/stats', JSON.stringify(stats))
+}
+
+
+function showStats ({ stars, forks }) {
+  if (stars) {
+    document.querySelector('.js-repo-stars').textContent = stars
   }
-  const last = localStorage.getItem('_time')
-  if (!last || Date.now() - parseInt(last, 10) > 86400000) {
+  if (forks) {
+    document.querySelector('.js-repo-forks').textContent = forks
+  }
+}
+
+
+function renderStats () {
+  const _stats = sessionStorage.getItem('_sy/repo/stats')
+  if (_stats) {
+    showStats(JSON.parse(_stats))
+  } else {
     const user = element.getAttribute('data-user')
     const repo = element.getAttribute('data-repo')
     const type = element.getAttribute('data-type')
     if (type === 'github') {
-      fetchGitHubStats(user + '/' + repo)
+      fetchGitHubStats(user, repo)
+    } else if (type === 'gitlab') {
+      fetchGitlabStats(user, repo)
     }
   }
+}
+
+if (element) {
+  renderStats()
 }
