22e7bcca05ff950e5b317d39609828a48938b358
Hsiaoming Yang <me@lepture.com>
2023-07-14 22:40:24 +0900

    feat: expand and collapse global toc

--- static/js/toc.js
+++ static/js/toc.js
@@ -12,6 +12,44 @@ function handleHash () {
 
 handleHash()
 
+function addToggleToc () {
+  const sublist = document.querySelectorAll(".globaltoc .toctree-l1 ul")
+  sublist.forEach(el => {
+    const list = el.parentNode
+    const link = el.previousSibling
+    const title = link.textContent
+    const button = document.createElement("button")
+    button.innerHTML = '<i class="i-icon chevron"></i>'
+
+    const updateButton = () => {
+      if (list.classList.contains("current")) {
+        button.setAttribute("arial-label", "Collapse " + title)
+      } else {
+        button.setAttribute("arial-label", "Expand " + title)
+      }
+    }
+    updateButton()
+
+    const toggleCurrent = (e) => {
+      e.preventDefault()
+      if (list.classList.contains("current")) {
+        list.classList.remove("current")
+      } else {
+        list.classList.add("current")
+      }
+      updateButton()
+    }
+
+    if (link.getAttribute("href") === "#") {
+      link.addEventListener("click", toggleCurrent)
+    }
+    button.addEventListener("click", toggleCurrent)
+    list.insertBefore(button, el)
+  })
+}
+
+addToggleToc()
+
 window.addEventListener('hashchange', () => {
   handleHash()
 }, false)
