69d5a522ae677583dffd318d5e751db7f95391b2
Hsiaoming Yang <me@lepture.com>
2025-07-24 09:39:51 +0900

    fix: improve accessibility for sub nav links, open sub nav links by keyboard

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,28 +1,65 @@
-function toggleMenu(button) {
+/**
+ * @param {HTMLButtonElement} button
+ */
+function bindButtonEvent(button) {
   const id = button.getAttribute('aria-controls')
-  const ctrl = document.getElementById(id)
-  const expanded = 'data-expanded-' + id
-  button.addEventListener('click', function () {
-    if (document.body.hasAttribute(expanded)) {
-      document.body.removeAttribute(expanded)
-      ctrl.classList.remove('_expanded')
+  const content = document.getElementById(id)
+  if (!content) return
+
+  content.addEventListener('click', (e) => {
+    e.stopPropagation()
+  })
+
+  button.addEventListener('click', (e) => {
+    e.stopPropagation()
+
+    const components = getExpandedComponents()
+    const index = components.indexOf(id)
+    if (content.getAttribute('aria-hidden') === 'false') {
+      components.splice(index, 1)
+      document.body.setAttribute('data-expanded', components.join(' '))
+      content.setAttribute('aria-hidden', 'true')
       setButtonExpanded(id, 'false')
     } else {
-      document.body.setAttribute(expanded, "true")
-      ctrl.classList.add('_expanded')
+      components.push(id)
+      document.body.setAttribute('data-expanded', components.join(' '))
+      content.setAttribute('aria-hidden', 'false')
       setButtonExpanded(id, 'true')
     }
   })
 }
 
+/**
+ * @param {string} id
+ * @param {string} value
+ */
 function setButtonExpanded(id, value) {
   const els = document.querySelectorAll('[aria-controls="' + id + '"]')
-  for (el of els) {
-    el.setAttribute('aria-expanded', value)
+  for (let i = 0; i < els.length; i++) {
+    els[i].setAttribute('aria-expanded', value)
   }
 }
 
+function getExpandedComponents() {
+  const expanded = document.body.getAttribute('data-expanded') || ''
+  if (!expanded.trim()) {
+    return []
+  }
+  return expanded.split(/\s+/)
+}
+
+/** @type {NodeListOf<HTMLButtonElement>} */
 const menuButtons = document.querySelectorAll('.js-menu')
 for (let i = 0; i < menuButtons.length; i++) {
-  toggleMenu(menuButtons[i])
+  bindButtonEvent(menuButtons[i])
 }
+
+document.body.addEventListener('click', () => {
+  const components = getExpandedComponents()
+  document.body.setAttribute('data-expanded', '')
+  components.forEach((id) => {
+    const content = document.getElementById(id)
+    content.setAttribute('aria-hidden', 'true')
+    setButtonExpanded(id, 'false')
+  })
+})
