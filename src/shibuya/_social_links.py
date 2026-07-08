51314a1529eeb334afe8bbad3177bf7ce3acc316
Hsiaoming Yang <me@lepture.com>
2026-07-08 17:37:34 +0900

    fix: social network theme.conf

---
+++ src/shibuya/_social_links.py
@@ -0,0 +1,60 @@
+from typing import Dict, Literal, Any
+
+
+SOCIAL_LABELS = {
+    "x": "X (Twitter)",
+    "github": "GitHub",
+    "youtube": "YouTube",
+}
+
+DEFAULT_NAV_SOCIALS = [
+    "github",
+    "gitlab",
+    "bitbucket",
+    "x",
+    "discord",
+    "slack",
+]
+
+DEFAULT_FOOT_SOCIALS = [
+    "readthedocs",
+    "github",
+    "gitlab",
+    "bitbucket",
+    "x",
+    "bluesky",
+    "mastodon",
+    "slack",
+    "discord",
+    "youtube",
+    "reddit",
+    "linkedin",
+]
+
+DEFAULT_SOCIALS = {
+    "theme_nav_socials": DEFAULT_NAV_SOCIALS,
+    "theme_foot_socials": DEFAULT_FOOT_SOCIALS,
+}
+
+
+def patch_social_context(context: Dict[str, Any]) -> None:
+    # patch twitter url
+    twitter_url = context.get("theme_twitter_url")
+    x_url = context.get("theme_x_url")
+    if twitter_url and not x_url:
+        context["theme_x_url"] = twitter_url
+
+    # extends social links
+    context["theme_nav_socials"] = list(_fix_social_links(context, "theme_nav_socials"))
+    context["theme_foot_socials"] = list(_fix_social_links(context, "theme_foot_socials"))
+
+
+def _fix_social_links(context: Dict[str, Any], key: Literal["theme_nav_socials", "theme_foot_socials"]):
+    fields = context.get(key)
+    if not fields:
+        fields = DEFAULT_SOCIALS[key]
+    for name in fields:
+        url = context.get(f"theme_{name}_url")
+        if url:
+            label = SOCIAL_LABELS.get(name, name.title())
+            yield dict(icon=f"simple-icons:{name}", url=url, label=label)
